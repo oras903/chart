489b51df311e9747343d20092bcaef13fa4d5e21
oras903 <352965446@qq.com>
2018-01-02 15:35:15 +0800

    修改中文显示

--- src/views/ChartLine.vue
+++ src/views/ChartLine.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
 <div>
-<button v-on:click="refreshCharts()">refreshh</button>
+<button v-on:click="refreshCharts()">refreshhh</button>
 <div class="line" id="line"></div>
 </div>
 </div>
